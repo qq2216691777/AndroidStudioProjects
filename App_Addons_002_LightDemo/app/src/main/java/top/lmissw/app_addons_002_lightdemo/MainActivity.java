6d66ed296d4e5bf7b9aad696cb5745d949f4f5ce
qq2216691777 <2216691777@qq.com>
2017-05-28 10:42:14 +0800

    Added support for backlighting on the original basis

--- App_Addons_002_LightDemo/app/src/main/java/top/lmissw/app_addons_002_lightdemo/MainActivity.java
+++ App_Addons_002_LightDemo/app/src/main/java/top/lmissw/app_addons_002_lightdemo/MainActivity.java
@@ -3,12 +3,16 @@ package top.lmissw.app_addons_002_lightdemo;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar mBackLightSeekBar = null;
+
     private Button mLightButton=null;
     boolean flashing = false;
     final  private int LED_NOTIFICATION_ID = 123;
@@ -30,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         mLightButton = (Button)findViewById(R.id.button);
         mLightButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +49,42 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 mLightHander.postDelayed(mLightRunnable,20000);//延时20000ms（20S） 开发板需要设置在15s后息屏
+
+            }
+        });
+
+        /* 关闭自动调光功能 */
+        Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+
+        mBackLightSeekBar = (SeekBar)findViewById(R.id.seekBar);
+
+        try {
+            int brightness = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
+            mBackLightSeekBar.setProgress(brightness*100/255);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        mBackLightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int brightness = mBackLightSeekBar.getProgress();
+                brightness = brightness*255/100;
+
+                android.provider.Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS,brightness);
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
         });
---
+++ App_Addons_002_LightDemo/readme.txt
@@ -0,0 +1,5 @@
+/**********************
+ *  操作背光需要添加权限
+ *  在 app -> manifests -> 的AndroidManifest.xml文件中添加 <uses-permission android:name="android.permission.WRITE_SETTINGS" />
+ *
+*/
